a8fb218050bc3037969208828b2650352b6f57ac
Svartingknas <svartingknas@gmail.com>
2017-03-15 18:45:39 -0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,22 @@
+function _(id){ return document.getElementById(id);}
+function submitForm(){
+  _("mybtn").disabled = true;
+  _("status").innerHTML = 'Please wait...';
+  var formdata = new FormData();
+  formdata.append("n", _("n").value);
+  formdata.append("e", _("e").value);
+  formdata.append("m", _("m").value);
+  var ajax = new XMLHttpRequest();
+  ajax.open("POST", "parser.php");
+  ajax.onreadystatechange = function(){
+    if (ajax.readyState == 4 && ajax.status == 200) {
+      if (ajax.responseText == "success") {
+        _("my_form").innerHTML = '<h2>Thanks! '+n.value+'Your message has been sent!'
+      } else {
+        _("status").innerHTML = ajax.responseText;
+        _("mybtn").disabled = false;
+      }
+    }
+  }
+  ajax.send(formdata);
+}
